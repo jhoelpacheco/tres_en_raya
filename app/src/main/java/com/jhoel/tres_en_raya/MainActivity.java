dbb047ae144e42fdcc0c742fdbd39f9cd5b343bb
jhoelpacheco <jhoel.pacheco12@gmail.com>
2022-01-12 15:35:13 +0100

    Initial commit

---
+++ app/src/main/java/com/jhoel/tres_en_raya/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jhoel.tres_en_raya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
